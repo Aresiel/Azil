5065a74eb7eb9faa701d668d59004133acb33ee7
Aresiel <love.dittlau@gmail.com>
2024-01-05 15:50:26 +0100

    new cat sound

--- src/commands/MeowCommand.ts
+++ src/commands/MeowCommand.ts
@@ -2,7 +2,7 @@ import { Message } from "discord.js";
 import { Command } from "../Command.js";
 
 class MeowCommand extends Command {
-    override trigger_words = ["meow", "miao", "mjau", "miau", "nya"];
+    override trigger_words = ["meow", "miao", "mjau", "miau", "nya", "mya"];
     override name = "Meow"
     
     override async execute(msg: Message<boolean>, trigger_word: string, args: string[]) {
