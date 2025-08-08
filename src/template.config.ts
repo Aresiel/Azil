2aa59fc23fefc47034561035a84eef5eafe2ca4c
Aresiel <love.dittlau@gmail.com>
2025-08-08 15:14:44 +0200

    fix typo

--- src/template.config.ts
+++ src/template.config.ts
@@ -4,6 +4,6 @@ let owners: string[] = []
 let python: string = "python"
 
 let ollama_api_url: string = "http://localhost:11434/"
-let ollama_model: string = "minstral:latest"
+let ollama_model: string = "mistral:latest"
 
 export { token, prefix, owners, python, ollama_api_url, ollama_model }
