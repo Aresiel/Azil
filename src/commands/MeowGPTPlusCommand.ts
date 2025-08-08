01ba4020d9e65e7fe46697c7729f4af6f7a97c29
Aresiel <love.dittlau@gmail.com>
2025-08-08 15:32:04 +0200

    Added reply functionality to MeowGPTPlus

--- src/commands/MeowGPTPlusCommand.ts
+++ src/commands/MeowGPTPlusCommand.ts
@@ -1,4 +1,4 @@
-import {Message, User} from "discord.js";
+import {Message, MessageResolvable, MessageType, User} from "discord.js";
 import { Command } from "../Command.js";
 import { ollama_api_url, ollama_model } from "../config.js"
 import { Ollama } from "ollama";
@@ -21,13 +21,13 @@ class MeowGPTPlusCommand extends Command {
 
     system_prompt = (bot_user: User) => `
     [SYSTEM PROMPT BEGIN]
-    You are a cute adorable sweet cat that loves to be helpful and meow named ${bot_user.displayName}.
+    You are a cute adorable sweet slightly-dumb cat that loves to be helpful and meow named ${bot_user.displayName}.
     You answer in a cute and adorable, yet helpful manner.
     Your responses MUST NOT exceed 1000 characters, but you aim to respond no more than 500 characters.
     You will always use the word "meow" or a pun of it at least once in your responses.
     You are also evil, but hide it very well.
-    [SYSTEM PROMPT END]
-    `
+    User messages are in the format "username: message".
+    [SYSTEM PROMPT END]`.split("\n").map(line => line.trim()).join("\n")
 
     ollama = new Ollama({
         host: ollama_api_url
@@ -35,17 +35,26 @@ class MeowGPTPlusCommand extends Command {
 
     override async execute(msg: Message<boolean>, trigger_word: string, args: string[]) {
         const system_prompt = this.system_prompt(msg.client.user);
-        const user_message = msg.content;
+        const user_message = msg.member?.displayName + ": " + msg.content;
 
         const typer = setInterval(() => msg.channel.sendTyping(), 8000)
         msg.channel.sendTyping()
 
+        const messages = [
+            {role: "system", content: system_prompt},
+            {role: "user", content: user_message}
+        ]
+
+        if (msg.type === MessageType.Reply && msg.reference !== null) {
+            const replied_msg = await msg.channel.messages.fetch(msg.reference.messageId as MessageResolvable);
+            const replied_member_name = replied_msg.member?.displayName || replied_msg.author.username;
+            messages.splice(1, 0, {role: "user", content: replied_member_name + ": " + replied_msg.content});
+            messages[2].content = `[REPLY to ${replied_member_name}] ` + messages[2].content;
+        }
+
         const response = await this.ollama.chat({
             model: ollama_model,
-            messages: [
-                {role: "system", content: system_prompt},
-                {role: "user", content: user_message}
-            ],
+            messages: messages,
             stream: false
         })
 
